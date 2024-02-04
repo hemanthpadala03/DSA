2d8599bb2d71b2d00840a60b67d5aff9c088acc5
hemanthpadala03 <127174929+hemanthpadala03@users.noreply.github.com>
2024-02-04 23:33:07 +0530

    Create Insertion_sort.c

---
+++ Insertion_sort.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void bubble_sort(int a[],int c){
+    for(int i=1;i<c;i++){
+        int key=a[i];
+        int j=i-1;
+        while(j>0 && key<a[j]){
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=key;
+    }
+}
+int main(){
+    int a[]={12,23,75,93,35,65};
+    int c=sizeof(a)/sizeof(a[0]);
+    bubble_sort(a,c);
+    for(int i=0;i<c;i++){
+        printf("%d ", a[i]);   
+    }
+    printf("\n");
+    return 0;
+}
